557c35f1c65f9bfc77413a2cbac41d0501b0195c
Vicente Sanchez <Vikom.sanchez@gmail.com>
2023-10-16 16:42:15 -0300

    billing form fix

--- src/components/BillingForm.tsx
+++ src/components/BillingForm.tsx
@@ -1,53 +1,80 @@
-"use client"
+'use client'
 
-import { trpc } from "@/app/_trpc/client"
-import { getUserSubscriptionPlan } from "@/lib/stripe"
-import { toast } from "sonner"
-import MaxWidthWrapper from "./MaxWidthWrapper"
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
-import { Button } from "./ui/button"
-import { Loader2 } from "lucide-react"
-import { format } from "date-fns"
+import { getUserSubscriptionPlan } from '@/lib/stripe'
+import { trpc } from '@/app/_trpc/client'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+import { Button } from './ui/button'
+import { Loader2 } from 'lucide-react'
+import { format } from 'date-fns'
+import { toast } from 'sonner'
 
 interface BillingFormProps {
-  subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>
+  subscriptionPlan: Awaited<
+    ReturnType<typeof getUserSubscriptionPlan>
+  >
 }
 
-const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
+const BillingForm = ({
+  subscriptionPlan,
+}: BillingFormProps) => {
 
-  const { mutate: createStripeSession, isLoading } = trpc.createStripeSession.useMutation({
-    onSuccess: (({ url }) => {
-      if (url) window.location.href = url
-      if (!url) toast.error('There was a problem...', {
-        description: 'Please try again in a moment',
-        duration: 5000
-      })
+  const { mutate: createStripeSession, isLoading } =
+    trpc.createStripeSession.useMutation({
+      onSuccess: ({ url }) => {
+        if (url) window.location.href = url
+        if (!url) {
+          toast.error('There was a problem...', {
+            description: 'Please try again in a moment',
+            duration: 5000
+          })
+        }
+      },
     })
-  })
 
   return (
     <MaxWidthWrapper className='max-w-5xl'>
-      <form className='mt-12' onSubmit={(e) => {
-        e.preventDefault()
-        createStripeSession()
-      }}>
+      <form
+        className='mt-12'
+        onSubmit={(e) => {
+          e.preventDefault()
+          createStripeSession()
+        }}>
         <Card>
           <CardHeader>
             <CardTitle>Subscription Plan</CardTitle>
-            <CardDescription>You are currently on the <strong>{subscriptionPlan.name}</strong> plan.</CardDescription>
+            <CardDescription>
+              You are currently on the{' '}
+              <strong>{subscriptionPlan.name}</strong> plan.
+            </CardDescription>
           </CardHeader>
 
-          <CardFooter className='flex flex-col items-start space-y-1 md:flex-row md:justify-between md:space-x-0'>
+          <CardFooter className='flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0'>
             <Button type='submit'>
               {isLoading ? (
                 <Loader2 className='mr-4 h-4 w-4 animate-spin' />
               ) : null}
-              {subscriptionPlan.isSubscribed ? 'Manage Subscription' : 'Upgrade to Pro'}
+              {subscriptionPlan.isSubscribed
+                ? 'Manage Subscription'
+                : 'Upgrade to PRO'}
             </Button>
 
             {subscriptionPlan.isSubscribed ? (
               <p className='rounded-full text-xs font-medium'>
-                {subscriptionPlan.isCanceled ? 'Your plan will be canceled on' : 'Your plan renews on'} {format(subscriptionPlan.stripeCurrentPeriodEnd!, 'dd.mm.yyyy')}.
+                {subscriptionPlan.isCanceled
+                  ? 'Your plan will be canceled on '
+                  : 'Your plan renews on'}
+                {format(
+                  subscriptionPlan.stripeCurrentPeriodEnd!,
+                  'dd.MM.yyyy'
+                )}
+                .
               </p>
             ) : null}
           </CardFooter>
@@ -55,7 +82,6 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
       </form>
     </MaxWidthWrapper>
   )
-
 }
 
 export default BillingForm
