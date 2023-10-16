05a4dc907d5c3ddeed79ae043a137de47cde0c6a
Vicente Sanchez <Vikom.sanchez@gmail.com>
2023-10-16 15:59:51 -0300

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-import { cn } from '@/lib/utils'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Providers from '@/components/Providers'
+import { cn, constructMetadata } from '@/lib/utils'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
 import "react-loading-skeleton/dist/skeleton.css"
 import "simplebar-react/dist/simplebar.min.css"
@@ -12,10 +11,7 @@ import { Toaster } from 'sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Quill',
-  description: 'The only way to chat with your documents',
-}
+export const metadata = constructMetadata()
 
 export default function RootLayout({
   children,
