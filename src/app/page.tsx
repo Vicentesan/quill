50f95495c0958b4ac5410313f57367c9176bc0f5
Vicente Sanchez <Vikom.sanchez@gmail.com>
2023-10-16 17:32:17 -0300

    Dynamic get started button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import Link from "next/link";
 import { ArrowRight } from 'lucide-react'
 import { buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 export default function Home() {
+  
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+  
   return (
     <>
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center ">
@@ -21,7 +26,7 @@ export default function Home() {
         <Link className={buttonVariants({
           size: 'lg',
           className: 'mt-5'
-        })} href='/sign-up'>
+        })} href={!user ? '/sign-up' : '/dashboard'}>
           Get started <ArrowRight className="ml-2 h-5 w-5 " />
         </Link>
       </MaxWidthWrapper>
