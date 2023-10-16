5bd62a01e4397ade7655c4a9fecae8ce52b4b826
Vicente Sanchez <Vikom.sanchez@gmail.com>
2023-10-16 12:08:55 -0300

    CardFooter

--- src/components/BillingForm.tsx
+++ src/components/BillingForm.tsx
@@ -4,7 +4,10 @@ import { trpc } from "@/app/_trpc/client"
 import { getUserSubscriptionPlan } from "@/lib/stripe"
 import { toast } from "sonner"
 import MaxWidthWrapper from "./MaxWidthWrapper"
-import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card"
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
+import { Button } from "./ui/button"
+import { Loader2 } from "lucide-react"
+import { format } from "date-fns"
 
 interface BillingFormProps {
   subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>
@@ -33,6 +36,21 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
             <CardTitle>Subscription Plan</CardTitle>
             <CardDescription>You are currently on the <strong>{subscriptionPlan.name}</strong> plan.</CardDescription>
           </CardHeader>
+
+          <CardFooter className='flex flex-col items-start space-y-1 md:flex-row md:justify-between md:space-x-0'>
+            <Button type='submit'>
+              {isLoading ? (
+                <Loader2 className='mr-4 h-4 w-4 animate-spin' />
+              ) : null}
+              {subscriptionPlan.isSubscribed ? 'Manege Subscription' : 'Upgrade to Pro'}
+            </Button>
+
+            {subscriptionPlan.isSubscribed ? (
+              <p className='rounded-full text-xs font-medium'>
+                {subscriptionPlan.isCanceled ? 'Your plan will be canceled on' : 'Your plan renews on'} {format(subscriptionPlan.stripeCurrentPeriodEnd!, 'dd.mm.yyyy')}.
+              </p>
+            ) : null}
+          </CardFooter>
         </Card>
       </form>
     </MaxWidthWrapper>
